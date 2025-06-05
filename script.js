1256ed0d68ab57aa2562133b4b5869d26cb4ebd5
Saniya Ahmad <130426902+Styx-3608@users.noreply.github.com>
2025-06-05 11:13:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
 function fibonacci(num) {
-function fibonacci(num) {
-  if (num === 0) return 0;
   if (num === 1) return 0;
   if (num === 2) return 1;
 
-  let a = 0, b = 1, next;
-
+  let a = 0, b = 1;
   for (let i = 3; i <= num; i++) {
-    next = a + b;
+    let temp = a + b;
     a = b;
-    b = next;
+    b = temp;
   }
-
   return b;
 }
-
-	
-module.exports = fibonacci;
